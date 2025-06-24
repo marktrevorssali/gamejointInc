afd496071755e5f946e87df2cbff0619f120618e
Mark Trevor Ssali <trevomark12@gmail.com>
2025-06-24 20:21:53 +0300

    edited-version

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ products.forEach(product => {
   const div = document.createElement("div");
   div.className = "product";
   div.innerHTML = `
-    <img src="${product.image}" alt="${product.name}">
+    <a href="product.html?id=${product.id}">
+    <img src="${product.image}" alt="${product.name}" class="on-display">
     <h3>${product.name}</h3>
     <p>Price: ${product.price}</p>
+    <button class = "detail-btn">View Details</button>
   `;
   productList.appendChild(div);
 });
--- products.js
+++ products.js
@@ -5,27 +5,28 @@ const products = [
         id: 1,
         name: 'PS5 plus 1 controller',
         price: '$300',
-        image: "images/ps5-1.jpg"
+        image: "images/ps5-1.jpg",
+        description:'uiwfhweif'
     },
 
      {
         id: 2,
         name: 'PlayStation 3 and Controller (Black)',
         price: '$100',
-        image: "images/ps3-1.jpg"
+        image: "images/ps3-1.jpg",
     },
 
      {
         id: 3,
         name: 'PlayStation 5 controller Fortnite limited edition',
         price: '$100',
-        image: "images/fortnite-ltd-pad-k.jpg"
+        image: "images/fortnite-ltd-pad-k.jpg",
     },
 
      {
         id: 3,
         name: 'PlayStation 5 controller Fortnite Limited edition',
         price: '$100',
-        image: "images/fortnite-ltd-pad-k.jpg"
+        image: "images/fortnite-ltd-pad-k.jpg",
     } 
 ];
