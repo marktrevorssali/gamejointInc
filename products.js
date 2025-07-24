b7ec0eabe136dcbb13c9176fda1b91a81c32002c
Mark Trevor Ssali <trevomark12@gmail.com>
2025-07-24 23:06:19 +0300

    updated as mini database for index

--- products.js
+++ products.js
@@ -4,6 +4,41 @@ const products = [
     {
         id: 1,
         name: 'PS5 plus 1 controller',
+        price: 'UGX 2,150,000',
+        image: "images/ps5-1.jpg",
+        gallery:[
+            "images/borderland-4.jpg",
+            "images/borderland-4.jpg",
+            "images/borderland-4.jpg",
+        ],
+        description:'This is a playStation 5 with one controller white in color. Its used but in perfectly working condition with a 2 months warranty',
+    },
+
+     {
+        id: 2,
+        name: 'PlayStation 3 and Controller (Black)',
+        price: '$100',
+        image: "images/ps3-1.jpg",
+        gallery:[
+             
+        ],
+        description:'',
+    },
+
+     {
+        id: 3,
+        name: 'PlayStation 5 controller Fortnite limited edition',
+        price: '$100',
+        image: "images/fortnite-ltd-pad-k.jpg",
+        gallery:[
+            
+        ],
+        description:'',
+    },
+
+     {
+        id: 1,
+        name: 'PS5 plus 1 controller',
         price: '$300',
         image: "images/ps5-1.jpg",
         gallery:[
@@ -20,7 +55,7 @@ const products = [
         price: '$100',
         image: "images/ps3-1.jpg",
         gallery:[
-            
+             
         ],
         description:'',
     },
